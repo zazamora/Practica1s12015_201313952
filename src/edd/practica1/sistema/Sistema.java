b5d39f057c965c04e6d78493b67c1eed1712c8ea
zazamora <s4ulpalac1os@gmail.com>
2015-02-21 20:27:37 -0600

    Practica prueba subida

---
+++ src/edd/practica1/sistema/Sistema.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edd.practica1.sistema;
+
+/**
+ *
+ * @author Saul
+ */
+public class Sistema {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Inicializa el menu principal.
+    }
+    
+}
